9e8f8a089712d61a05175c5f01a9c75659942a23
Aaron Drabeck <aaron-drabeck@leavitt.com>
2017-07-10 17:17:50 -0600

    fixes duplicate path

--- polymer2-ts.ts
+++ polymer2-ts.ts
@@ -1,5 +1,4 @@
 /// <reference path="../reflect-metadata/Reflect.d.ts" />
-/// <reference path="./bower_components/reflect-metadata/Reflect.d.ts" />
 
 function observe(targets: string | string[]) {
     return (proto: any, propName: string): any => {
